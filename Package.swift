49895f94476902571ac1cf40bb98c79caf109f23
Ryan Carver <ryan@ryancarver.com>
2026-02-11 20:28:10 -0800

    bump to macos 12 for task modifier

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
   name: "sqlite-undo",
   platforms: [
     .iOS(.v13),
-    .macOS(.v11),
+    .macOS(.v12),
   ],
   products: [
     .library(name: "SQLiteUndo", targets: ["SQLiteUndo"]),
